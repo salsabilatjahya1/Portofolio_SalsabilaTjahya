f83292d982de55d8f77a53b8b141d5fb840ad344
salsabilatjahya1 <slsbilatjahya15@gmail.com>
2024-07-31 14:44:06 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+document.getElementById("portfolio-link1").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "https://play.google.com/store/search?q=taniland&c=apps"; // Arahkan ke link href
+    }); 
+
+document.getElementById("portfolio-link2").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "https://public.tableau.com/app/profile/salsa.putri2156/vizzes"; // Arahkan ke link href
+    }); 
+
+document.getElementById("portfolio-link3").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "https://public.tableau.com/app/profile/salsa.putri2156/vizzes"; // Arahkan ke link href
+    });
